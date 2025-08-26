f003ccffe9051e52ff9149bfbf9019b93f52f55f
= <=>
2025-08-26 22:46:23 +0300

    init angroid config for android studio develop

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'fitminiapp.com',
+  appName: 'fitness-miniapp',
+  webDir: 'www'
+};
+
+export default config;
