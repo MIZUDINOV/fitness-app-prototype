60d15dad7a47a8918ba3ad3ad2a0d16cca79737c
= <=>
2025-08-29 21:57:57 +0300

    init: создана минимальная структура входной точки App

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project_f/widgets/app.dart';
 
 void main() {
-  runApp(const MainApp());
-}
+  const app = App();
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
+  runApp(app);
 }
